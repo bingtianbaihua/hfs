f372397737e683026f2e8f0eb5ec42d881a72c64
bingtianbaihua <bintianbaihua@gmail.com>
2019-02-21 11:41:09 +0800

    add doc

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HFS
-golang 文件服务器
+文件服务器, 类似于 python -m SimpleHTTPServer port
+
+## Usage
+```
+-dir string
+    http file directory (default "./")
+-host string
+    ip address (default "0.0.0.0")
+-port string
+    listen http port (default "8910")
+-prefix string
+    http route path (default "/")
+-v string
+    release version
+```
--- cmd/main.go
+++ cmd/main.go
@@ -9,34 +9,22 @@ import (
 )
 
 const (
-	helpStr = `
-fs version: fs/0.01
-Usage: fs [-?h] [-host ip address] [-port port] [-path http handler path] [-dir file directory]
-
-Options:
-  -?,-h         : this help
-  -v            : show version and exit
-  -V            : show version and configure options then exit
-  -host         : fs work work address, default: 0.0.0.0
-  -port			: fs listen http port
-  -prefix       : http handler path
-  -dir			: fileserver directory
-`
+	version = "0.0.1"
 )
 
 var (
-	host, port, prefix, dir, help string
+	host, port, prefix, dir, v string
 )
 
 func main() {
-	flag.StringVar(&help, "h", "", helpStr)
+	flag.StringVar(&v, "v", "", "release version")
 	flag.StringVar(&host, "host", "0.0.0.0", "ip address")
 	flag.StringVar(&port, "port", "8910", "listen http port")
 	flag.StringVar(&prefix, "prefix", "/", "http route path")
 	flag.StringVar(&dir, "dir", "./", "http file directory")
 
 	flag.Parse()
-	fmt.Println(host, port, prefix, dir)
+	fmt.Printf("address: %v, port: %v, url prefix: %v, serve directory: %v\n", host, port, prefix, dir)
 
 	cfg := &server.Config{
 		Host:   host,
